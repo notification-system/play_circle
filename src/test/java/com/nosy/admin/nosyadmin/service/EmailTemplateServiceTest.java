7768a97c4f107e3366d41b62a79c6ea8298dd7f8
Oktay <oktay.alizada@rectasolutions.com>
2019-05-30 00:25:31 +0200

    add EmailTemplateServiceTest

--- src/test/java/com/nosy/admin/nosyadmin/service/EmailTemplateServiceTest.java
+++ src/test/java/com/nosy/admin/nosyadmin/service/EmailTemplateServiceTest.java
@@ -152,6 +152,7 @@ public class EmailTemplateServiceTest {
         when(emailTemplateRepositoryMock.findEmailTemplatesByInputSystemId(inputSystemId)).thenReturn(listOfEmailTemplates);
         assertEquals(listOfEmailTemplates,emailTemplateServiceMock.getListOfEmailTemplates(inputSystemId, email));
         assertEquals("testInputSystem", emailTemplate.getInputSystem().getInputSystemName());
+        assertEquals("Test", emailProviderProperties.getUsername());
     }
 
     @Test
---
+++ src/test/java/com/nosy/admin/nosyadmin/service/InputSystemServiceTest.java
@@ -0,0 +1,24 @@
+package com.nosy.admin.nosyadmin.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InputSystemServiceTest {
+
+    @Test
+    public void getListOfInputSystems() {
+    }
+
+    @Test
+    public void deleteInputSystem() {
+    }
+
+    @Test
+    public void saveInputSystem() {
+    }
+
+    @Test
+    public void updateInputSystemStatus() {
+    }
+}
