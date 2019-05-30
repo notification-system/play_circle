9bf75919b17f3ece6e086d13889d6c03b48bc4a5
Oktay <oktay.alizada@rectasolutions.com>
2019-05-30 19:02:12 +0200

    Add more tests for InputSystemService

--- src/test/java/com/nosy/admin/nosyadmin/service/InputSystemServiceTest.java
+++ src/test/java/com/nosy/admin/nosyadmin/service/InputSystemServiceTest.java
@@ -3,6 +3,7 @@ package com.nosy.admin.nosyadmin.service;
 import com.nosy.admin.nosyadmin.exceptions.GeneralException;
 import com.nosy.admin.nosyadmin.model.InputSystem;
 import com.nosy.admin.nosyadmin.model.User;
+import com.nosy.admin.nosyadmin.repository.InputSystemRepository;
 import com.nosy.admin.nosyadmin.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,6 +23,10 @@ public class InputSystemServiceTest {
 
     @Mock
     private UserRepository userRepositoryMock;
+
+    @Mock
+    private InputSystemRepository inputSystemRepository;
+
 
     User user;
 
@@ -68,8 +73,43 @@ public class InputSystemServiceTest {
         assertEquals(inputSystemList, inputSystemServiceMock.getListOfInputSystems(email));
 
     }
-    @Test
+    @Test(expected = Test.None.class)
     public void deleteInputSystem() {
+
+    /*
+    InputSystem checkInputSystem = inputSystemRepository.findByIdAndEmail(email, inputSystemId);
+    if (checkInputSystem == null) {
+      throw new GeneralException(MessageError.NO_INPUT_SYSTEM_FOUND.getMessageText());
+    }
+    if (!checkInputSystem.getEmailTemplate().isEmpty()) {
+      throw new GeneralException(MessageError.INPUT_SYSTEM_HAS_CHILDREN.getMessageText());
+    }
+    inputSystemRepository.deleteById(inputSystemId);
+     */
+        String email="test@nosy.tech";
+        InputSystem inputSystem=new InputSystem();
+        inputSystem.setInputSystemId("inputSystemId");
+        inputSystem.setInputSystemName("inputSystemName");
+        when(inputSystemRepository.findByIdAndEmail(email,inputSystem.getInputSystemId())).
+                thenReturn(inputSystem);
+        inputSystemServiceMock.deleteInputSystem(inputSystem.getInputSystemId(), email);
+
+
+
+
+    }
+
+
+    @Test(expected = GeneralException.class)
+    public void deleteInputSystemNotFound() {
+        String email="test@nosy.tech";
+        InputSystem inputSystem=new InputSystem();
+        inputSystem.setInputSystemId("inputSystemId");
+        inputSystem.setInputSystemName("inputSystemName");
+        when(inputSystemRepository.findByIdAndEmail(email,inputSystem.getInputSystemId())).
+                thenReturn(null);
+        inputSystemServiceMock.deleteInputSystem(inputSystem.getInputSystemId(), email);
+
     }
 
     @Test
