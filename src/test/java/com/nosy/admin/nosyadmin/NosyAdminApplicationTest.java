82e2c769a0e83b1df386efa8df3e421f20e996b7
Oktay <oktay.alizada@rectasolutions.com>
2019-05-31 00:18:50 +0200

    add more test cases for Userservice

---
+++ src/test/java/com/nosy/admin/nosyadmin/NosyAdminApplicationTest.java
@@ -0,0 +1,12 @@
+package com.nosy.admin.nosyadmin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NosyAdminApplicationTest {
+
+    @Test
+    public void main() {
+    }
+}
