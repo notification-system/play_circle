cf21e80398877852a59c679acf3dff053c9282ea
Oktay <oktay.alizada@rectasolutions.com>
2019-05-31 01:23:01 +0200

    moving from lombok to stard getter and setter

--- src/main/java/com/nosy/admin/nosyadmin/dto/InputSystemDto.java
+++ src/main/java/com/nosy/admin/nosyadmin/dto/InputSystemDto.java
@@ -10,12 +10,43 @@ import lombok.Setter;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
-@Getter
-@Setter
+
 public class InputSystemDto {
   @NotNull private String inputSystemId;
   @NotNull private String inputSystemName;
   @JsonBackReference private User user;
 
   @JsonManagedReference private Set<EmailTemplate> emailTemplate;
+
+  public String getInputSystemId() {
+    return inputSystemId;
+  }
+
+  public void setInputSystemId(String inputSystemId) {
+    this.inputSystemId = inputSystemId;
+  }
+
+  public String getInputSystemName() {
+    return inputSystemName;
+  }
+
+  public void setInputSystemName(String inputSystemName) {
+    this.inputSystemName = inputSystemName;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public Set<EmailTemplate> getEmailTemplate() {
+    return emailTemplate;
+  }
+
+  public void setEmailTemplate(Set<EmailTemplate> emailTemplate) {
+    this.emailTemplate = emailTemplate;
+  }
 }
