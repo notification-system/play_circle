2f4617f7567a5b2aa3c05d20ae8bf16ce3b1c539
Oktay <oktay.alizada@rectasolutions.com>
2019-06-01 01:50:54 +0200

    RestResponseEntityExceptionController Test

--- src/test/java/com/nosy/admin/nosyadmin/controller/EmailAdminControllerTest.java
+++ src/test/java/com/nosy/admin/nosyadmin/controller/EmailAdminControllerTest.java
@@ -1,28 +1,19 @@
 package com.nosy.admin.nosyadmin.controller;
 
 import com.nosy.admin.nosyadmin.dto.InputSystemDto;
-import com.nosy.admin.nosyadmin.model.EmailProviderProperties;
 import com.nosy.admin.nosyadmin.model.EmailTemplate;
-import com.nosy.admin.nosyadmin.model.InputSystem;
 import com.nosy.admin.nosyadmin.service.EmailTemplateService;
-import com.nosy.admin.nosyadmin.service.EmailTemplateServiceTest;
 import com.nosy.admin.nosyadmin.service.InputSystemService;
-import com.nosy.admin.nosyadmin.utils.InputSystemMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
-import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
-import java.util.Set;
 
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
