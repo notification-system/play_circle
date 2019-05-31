3ed6012ec51ac7153594f00beda8af9b8f672761
Oktay <oktay.alizada@rectasolutions.com>
2019-05-31 20:28:08 +0200

    Add EmailTemplateTest

--- src/test/java/com/nosy/admin/nosyadmin/utils/EmailTemplateMapperTest.java
+++ src/test/java/com/nosy/admin/nosyadmin/utils/EmailTemplateMapperTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class EmailTemplateMapperTest {
     private EmailTemplateDto emailTemplateDto=new EmailTemplateDto();
