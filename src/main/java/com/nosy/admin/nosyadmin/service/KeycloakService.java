75993910516216fa8d611f903302a99d7ead6750
Oktay <oktay.alizada@rectasolutions.com>
2019-06-01 19:07:07 +0200

    externalizing client role

--- src/main/java/com/nosy/admin/nosyadmin/service/KeycloakService.java
+++ src/main/java/com/nosy/admin/nosyadmin/service/KeycloakService.java
@@ -93,7 +93,7 @@ public class KeycloakService {
                                 .clients()
                                 .get(clientRep.getId())
                                 .roles()
-                                .get("nosy-role")
+                                .get(keycloakConfigBean.getNosyClientRole())
                                 .toRepresentation();
                 realmResource
                         .users()
