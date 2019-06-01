3bfa6d5eb32765c759602a5a0597342bb48174ce
Oktay <oktay.alizada@rectasolutions.com>
2019-06-01 18:57:45 +0200

    test for KeycloakService

--- src/test/java/com/nosy/admin/nosyadmin/service/KeycloakServiceTest.java
+++ src/test/java/com/nosy/admin/nosyadmin/service/KeycloakServiceTest.java
@@ -1,19 +1,31 @@
 package com.nosy.admin.nosyadmin.service;
 
 import com.nosy.admin.nosyadmin.config.KeycloakConfigBean;
+import com.nosy.admin.nosyadmin.config.security.ClientToken;
+import com.nosy.admin.nosyadmin.model.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.keycloak.admin.client.resource.RealmResource;
-import org.keycloak.admin.client.resource.UserResource;
-import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.admin.client.resource.*;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import javax.ws.rs.core.Response;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class KeycloakServiceTest {
@@ -22,6 +34,20 @@ public class KeycloakServiceTest {
 
     @Mock
     KeycloakConfigBean keycloakConfigBean;
+
+
+    private String email="test@nosy.tech";
+    private User user;
+    @Before
+    public void setUser(){
+
+        user=new User();
+        user.setEmail(email);
+        user.setFirstName("Test");
+        user.setLastName("Nosy");
+        user.setInfo("TestNosy");
+        user.setPassword("dajsndjasn");
+    }
 
     @Test
     public void isAuthenticatedFalse() {
@@ -86,14 +112,60 @@ public class KeycloakServiceTest {
 
 
     @Test
-    public void registerNewUser() {
+    public void registerNewUserFail() throws URISyntaxException {
+        User user=new User();
+        RealmResource realmResource=mock(RealmResource.class);
+        UsersResource usersResource=mock(UsersResource.class);
+        when(keycloakConfigBean.getKeycloakUserResource()).thenReturn(realmResource);
+        when(realmResource.users()).thenReturn(usersResource);
+        URI uri=new URI("dasdas");
+        Response response=Response.status(Response.Status.CONFLICT).
+                location(uri).build();
+        when(usersResource.create(any())).thenReturn(response);
+        assertFalse(keycloakService.registerNewUser(user));
+    }
+
+
+
+    @Test
+    public void registerNewUserSuccessWithClient() throws URISyntaxException {
+        User user=new User();
+        RealmResource realmResource=mock(RealmResource.class);
+        UsersResource usersResource=mock(UsersResource.class);
+        when(keycloakConfigBean.getKeycloakUserResource()).thenReturn(realmResource);
+        when(realmResource.users()).thenReturn(usersResource);
+        URI uri=new URI("dasdas");
+        Response response=Response.status(Response.Status.CREATED).
+                location(uri).build();
+        when(usersResource.create(any())).thenReturn(response);
+        ClientRepresentation clientRepresentation=mock(ClientRepresentation.class);
+        List<ClientRepresentation> clientRepresentationList=new ArrayList<ClientRepresentation>();
+        clientRepresentationList.add(clientRepresentation);
+        ClientResource clientResource=mock(ClientResource.class);
+        ClientsResource clientsResource=mock(ClientsResource.class);
+        RoleMappingResource roleMappingResource=mock(RoleMappingResource.class);
+        RolesResource rolesResource=mock(RolesResource.class);
+        RoleResource roleResource=mock(RoleResource.class);
+        RoleScopeResource roleScopeResource=mock(RoleScopeResource.class);
+        RoleRepresentation roleRepresentation=mock(RoleRepresentation.class);
+        UserResource userResource=mock(UserResource.class);
+        when(realmResource.clients()).thenReturn(clientsResource);
+        when(clientsResource.findByClientId(any())).thenReturn(clientRepresentationList);
+        when(clientsResource.get(any())).thenReturn(clientResource);
+        when(clientResource.roles()).thenReturn(rolesResource);
+        when(rolesResource.get(anyString())).thenReturn(roleResource);
+        when(roleResource.toRepresentation()).thenReturn(roleRepresentation);
+        when(usersResource.get(anyString())).thenReturn(userResource);
+        when(userResource.roles()).thenReturn(roleMappingResource);
+        when(roleMappingResource.clientLevel(any())).thenReturn(roleScopeResource);
+        doNothing().when(roleScopeResource).add(any());
+        assertTrue(keycloakService.registerNewUser(user));
     }
 
     @Test
-    public void requestInterceptor() {
-    }
-
-    @Test
-    public void getTokens() {
+    public void getTokens() throws IOException {
+        ClientToken clientToken=mock(ClientToken.class);
+        when(keycloakConfigBean.getTokens(user)).thenReturn(clientToken);
+        assertNotNull(keycloakService.getTokens(user));
     }
 }
