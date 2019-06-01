6a1c07ef51dce43b79e6e5b0c17ceed6ec95ee3a
Oktay <oktay.alizada@rectasolutions.com>
2019-06-01 21:38:21 +0200

    Fixing code smell

--- src/main/java/com/nosy/admin/nosyadmin/utils/InputSystemMapper.java
+++ src/main/java/com/nosy/admin/nosyadmin/utils/InputSystemMapper.java
@@ -11,16 +11,13 @@ import org.mapstruct.factory.Mappers;
 public abstract class InputSystemMapper {
 
     public static final InputSystemMapper INSTANCE = Mappers.getMapper( InputSystemMapper.class );
-    @Mappings({
-            @Mapping(source = "inputSystemId", target = "id"),
+
+            @Mapping(source = "inputSystemId", target = "id")
             @Mapping(source = "inputSystemName", target = "name")
 
-    })
     public abstract InputSystemDto toInputSystemDto(InputSystem inputSystem);
-    @Mappings({
-            @Mapping(source = "id", target = "inputSystemId"),
+            @Mapping(source = "id", target = "inputSystemId")
             @Mapping(source = "name", target = "inputSystemName")
 
-    })
     public abstract InputSystem toInputSystem(InputSystemDto emailTemplateDto);
 }
