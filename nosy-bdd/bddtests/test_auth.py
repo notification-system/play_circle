9a2f87102d38af9bd8261fd02e7a8344e8618bdd
Oktay <oktay.alizada@rectasolutions.com>
2019-06-16 23:04:24 +0200

    Add api tests for EmailAdminController

--- nosy-bdd/bddtests/test_auth.py
+++ nosy-bdd/bddtests/test_auth.py
@@ -9,6 +9,10 @@ status_token_url = 'http://localhost:8081/api/v1/nosy/auth/status'
 
 logout_token_url = 'http://localhost:8081/api/v1/nosy/auth/logout'
 
+create_inputsystemdto_url = 'http://localhost:8081/api/v1/nosy/inputsystems'
+
+get_emailproviders_url = 'http://localhost:8081/api/v1/nosy/inputsystems/emailproviders'
+
 api_user_create = {
     'firstName': 'testFirstName',
     'lastName': 'testLastName',
@@ -22,41 +26,46 @@ api_user_get = {
     'email': 'adasddddas@nosy.tech',
 
 }
+
+create_inputsystemdto = {
+    'name': 'inputsystemTest'
+}
+
 headers = {'Content-type': 'application/json'}
 
 
-# def test_user_creation():
-#     print('Get Text response')
-#     r = requests.post(url=create_user_url, data=json.dumps(api_user_create), headers=headers)
-#     jsonResult = r.json()
-#     assert jsonResult.get('email') == api_user_create.get('email')
-#     assert r.status_code == 201
+def test_auth_user_creation():
+    print('Get Text response')
+    r = requests.post(url=create_user_url, data=json.dumps(api_user_create), headers=headers)
+    json_result = r.json()
+    assert json_result.get('email') == api_user_create.get('email')
+    assert r.status_code == 201
 
 
-def test_conflict_user_creation():
+def test_auth_conflict_user_creation():
     r = requests.post(url=create_user_url, data=json.dumps(api_user_create), headers=headers)
     assert r.status_code == 409
 
 
-def test_get_token():
+def test_auth_get_token():
     r = requests.post(url=get_token_url, data=json.dumps(api_user_create), headers=headers)
     assert r.status_code == 200
     assert r.json is not None
 
 
 def test_get_status():
-    json_token = get_token()
+    json_token = auth_get_token()
     r = requests.post(url=status_token_url, data=json.dumps(json_token), headers=headers)
     assert r.text == 'true'
 
 
-def get_token():
+def auth_get_token():
     r = requests.post(url=get_token_url, data=json.dumps(api_user_create), headers=headers)
     return r.json()
 
 
-def test_get_user_profile():
-    json_token = get_token()
+def test_auth_get_user_profile():
+    json_token = auth_get_token()
     json_bearer = 'Bearer ' + json_token.get('accessToken')
     headers_auth = {'Content-type': 'application/json', 'Authorization': json_bearer}
     r = requests.get(url=create_user_url, headers=headers_auth)
@@ -66,9 +75,30 @@ def test_get_user_profile():
     assert user_profile.get('email') == api_user_get.get('email')
 
 
-def test_logout():
-    json_token = get_token()
+def test_auth_logout():
+    json_token = auth_get_token()
     json_bearer = 'Bearer ' + json_token.get('accessToken')
     headers_auth = {'Content-type': 'application/json', 'Authorization': json_bearer}
     r = requests.get(url=logout_token_url, headers=headers_auth)
     assert r.status_code == 204
+
+
+def test_email_admin_inputsystems_create():
+    json_token = auth_get_token()
+    json_bearer = 'Bearer ' + json_token.get('accessToken')
+    headers_auth = {'Content-type': 'application/json', 'Authorization': json_bearer}
+    r = requests.post(url=create_inputsystemdto_url, data=json.dumps(create_inputsystemdto), headers=headers_auth)
+    created_input_system = r.json()
+    assert r.status_code == 201
+    assert created_input_system.get('name') == create_inputsystemdto.get('name')
+
+
+def test_email_admin_get_emailproviders():
+    json_token = auth_get_token()
+    json_bearer = 'Bearer ' + json_token.get('accessToken')
+    headers_auth = {'Content-type': 'application/json', 'Authorization': json_bearer}
+    r = requests.get(url=get_emailproviders_url, headers=headers_auth)
+    assert r.status_code == 200
+    assert r.text == '["DEFAULT","YANDEX","GMAIL"]'
+
+def 
